189f508f765e82ba9ac9c833d16f572c5af4c378
jeffplaisance <jeffplaisance@gmail.com>
2024-07-20 00:41:10 +0000

    Deploying to gh-pages from  @ 9d8417a8dc832755fdcbb25e156ae590f17933fd 🚀

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['dec_5findex_618',['dec_index',['../structbpptree_1_1detail_1_1InternalNodeBase.html#afff0b3a29adb594edacf508e9d6fd5de',1,'bpptree::detail::InternalNodeBase']]],
-  ['depth_619',['depth',['../structbpptree_1_1detail_1_1BppTreeDetail_1_1Shared.html#a3b0dd3d0810843828be3f1a1146f6793',1,'bpptree::detail::BppTreeDetail::Shared']]],
+  ['depth_619',['depth',['../structbpptree_1_1detail_1_1BppTreeDetail_1_1Shared.html#a56b00c349e04023858f8922dae942bfb',1,'bpptree::detail::BppTreeDetail::Shared']]],
   ['destruct_620',['destruct',['../structbpptree_1_1detail_1_1UninitializedArray.html#a1e3a9cdbe211ef982a2066b300835dbc',1,'bpptree::detail::UninitializedArray']]],
   ['dispatch_621',['dispatch',['../structbpptree_1_1detail_1_1BppTreeDetail_1_1Shared.html#a0e9c4db029ba6fb7412b6f1e0fd38034',1,'bpptree::detail::BppTreeDetail::Shared::dispatch(Operation &amp;&amp;op, Us &amp;&amp;... us)'],['../structbpptree_1_1detail_1_1BppTreeDetail_1_1Shared.html#a89ec149ec6e875c648b0036dd508ecfe',1,'bpptree::detail::BppTreeDetail::Shared::dispatch(Operation const &amp;op, Us const &amp;... us) const']]],
   ['doerase_622',['DoErase',['../structbpptree_1_1detail_1_1InternalNodeBase_1_1DoErase.html#abfe5d2eb29c2e97116496e0efc52acaf',1,'bpptree::detail::InternalNodeBase::DoErase::DoErase()'],['../structbpptree_1_1detail_1_1ModifyTypes_1_1DoErase.html#a531822c20a3d8536683500d3e4c6db76',1,'bpptree::detail::ModifyTypes::DoErase::DoErase()']]],
